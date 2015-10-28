0b87d052f82805a01aaaff873c14cb01b6eb12a5
esvlad <svladpro@yandex.ru>
2015-10-28 23:19:38 +0500

    Проверка количества символов
    Сделал проверку количества вводимых символов, если текстовое поле
    пустое, то вызывается диалоговое окно с ошибкой и расчеты не
    производятся.

--- src/lenghttext/Main.java
+++ src/lenghttext/Main.java
@@ -5,11 +5,17 @@
  */
 package lenghttext;
 
+//Имортируем компонент JOptionPane пакета javax.swing
+import javax.swing.JOptionPane;
+
 /**
- * @author Влад
+ * @author Старцев Владислав, Рустем Еникеев, Игорь Мезенцев, Рустам Хайруллин
  */
+
+//Инициализируем главный класс программы
 public class Main extends javax.swing.JFrame {
     
+    //Инициализируем переменные форм
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
@@ -20,13 +26,15 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextPane jTextPane1;
     
+    //В главном методе инициализируем сгенерированные компоненты форм
     public Main() {
         initComponents();
     }
     /**
      * @param args the command line arguments
      */
-
+    //Инициализируем статический метод в котором запускаем главный метод Main
+    //для отрисовки формы
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -35,7 +43,11 @@ public class Main extends javax.swing.JFrame {
         });
     }
     
+    //Указываем компилятору подавить предупреждения компиляции, 
+    //определённые в параметрах аннотации
     @SuppressWarnings("unchecked")
+    
+    //Инициализируем компоненты формы
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
@@ -48,12 +60,15 @@ public class Main extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Текстовычислятор");
-       // setLocation(50, 50);
-        //setSize(480/1*2, 480);
         
+        //Указываем интерпретатору, что при зиакрытии окна
+        //нужно выйти из приложения
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        
+        //Устанавливаем Title фрейма
+        setTitle("Текстовычислятор");
+        
+        //Устанавливаем необходимые формы на панеле
         jScrollPane1.setViewportView(jTextPane1);
 
         jLabel1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
@@ -61,6 +76,8 @@ public class Main extends javax.swing.JFrame {
 
         jButton1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         jButton1.setText("Рассчитать");
+        //Прописываем, какой нужно использовать метод при нажатии на кнопку
+        //Расчитать
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -69,6 +86,8 @@ public class Main extends javax.swing.JFrame {
 
         jButton2.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         jButton2.setText("Сбросить текст");
+        //Прописываем, какой нужно использовать метод при нажатии на кнопку
+        //Сбросить текст
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
@@ -136,35 +155,57 @@ public class Main extends javax.swing.JFrame {
         );
 
         pack();
+        
+        //Указываем, что при открытии приложения, нужно его центровать 
         setLocationRelativeTo(null);
     }
     
-    //private JLabel slova;
+    //Инициализируем метод для обработки резутата после нажатия кнопки "Расчитать"
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        // TODO add your handling code here:
+        //Создаем переменные str тип String, a тип int
         String str;
         int a;
         
+        //Записываем текст из формы в переменную str
         str = jTextPane1.getText();
+        //Из переменной str удаляем лишние пробелы (с начала и конца текста)
         str = str.trim();
         
+        //Используя метод length считаем общее количество символов 
+        //из переменной str и записываем в переменную a
         a = str.length();
         
+        //Создаем переменную words и присваиваем ей тип String и указываем,
+        //что данная строка будет массивом, созданным с помощью
+        //метода split разбивая текст на слова через регулярное выражение
+        //из переменной str
         String[] words = str.split("[\\s,.:!?-]+");
-        /*
-        for (String w : words) {
-            System.out.println(w);
-        }*/
-       // System.out.println(words.length);
-       // System.out.println(a);
         
-        jLabel4.setText(""+a);
-        jLabel5.setText(""+words.length);
+        //Делаем проверку на количество символов
+        //Если пользователь не ввел ни одного символа и нажал на кнопку 
+        //Расчитать, то выводим сообщение об ошибке, при а равным нулю
+        if(a==0){
+            //Выводим мообщение об ошибке с помощью метода showMessageDialog
+            JOptionPane.showMessageDialog(null,"Вы не ввели текст!","Ошибка" ,
+            JOptionPane.ERROR_MESSAGE);
+        }
+        //Либо печаем в метках количество символов/слов
+        else{
+            //Заполняем метку jLabel4 общим количеством символов
+            jLabel4.setText(""+a);
+            //Заполняем метку jLabel5 количеством слов, в данном примере 
+            //метод length считает не количество символов, а количество 
+            //элементов массива words
+            jLabel5.setText(""+words.length);
+        }
+        
         
     }                                        
-
+    
+    //Инициализируем метод для сброса текста в текстовом поле
+    //А также стираем количество символов/слов
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        // TODO add your handling code here:
+        //Устанавливаем для jTextPane1, jLabel4, jLabel5 пустое значение:
         jTextPane1.setText("");
         jLabel4.setText("");
         jLabel5.setText("");
